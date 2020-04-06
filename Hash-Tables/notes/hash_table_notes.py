58bb4df0e72ea830003ee62be093036529c1a817
tobias-fyi <tobyreaper@gmail.com>
2020-04-06 12:04:46 -0600

    Hash tables 1 lecture

---
+++ Hash-Tables/notes/hash_table_notes.py
@@ -0,0 +1,13 @@
+"""
+Hash tables :: Day 1 notes - Hash tables
+
+* Hash tables have an underlying Array structure
+    * This is why they are as hast at accessing elements as arrays
+    * The main difference is that hash tables are indexed using a hash function
+    in an unordered fashion, whereas arrays are indexed via contiguous ordered indices
+* HashTable code is fairly similar to DynamicArray code
+"""
+
+# Dictionary is Python's built-in hash table
+# The underlying data struct is a hash table
+d = dict()
