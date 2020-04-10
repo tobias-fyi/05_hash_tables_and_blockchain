60948b2581d29e7c0be5409c58a1c52c79466bba
tobias-fyi <tobyreaper@gmail.com>
2020-04-09 18:38:04 -0600

    MVP functionality - base for CLI

--- Blockchain/client_mining_p/wallet.py
+++ Blockchain/client_mining_p/wallet.py
@@ -11,20 +11,26 @@ Blockchain — Day 2 Project :: Wallet app
 * Paginate the list of transactions if there are more than ten
 """
 
-# %%
 import json
+from pprint import pprint
 import sys
 
-# from flask import Flask, jsonify, request
 import requests
 
 
-# %%
 class User:
-    def __init__(self, user_id: str = "007", url: str = "http://localhost:5000"):
+    def __init__(
+        self,
+        user_id: str = "007",
+        balance: float = 0.0,
+        url: str = "http://localhost:5000",
+    ):
         """Constructor for the user account management handler class."""
         self.user_id = user_id
         self.url = url
+        self.cache = []
+        self.last_index = 0
+        self.balance = balance
 
     @property
     def user_id(self) -> str:
@@ -37,9 +43,25 @@ class User:
         self._user_id = new_id
         print(f"User ID: {new_id}")
 
+    @property
+    def balance(self) -> None:
+        """User's current number of coins."""
+        return self._balance
+
+    @balance.setter
+    def balance(self, new_id) -> None:
+        """Setter function for balance property."""
+        self._balance = new_id
+
+    def show_transactions(self) -> None:
+        """Displays user's transactions."""
+        pprint(self.cache)
+
     def post_transaction(self, recipient: str, amt: float) -> int:
         """Posts a new transaction from the user.
         Returns index of block into which transaction was posted."""
+        # TODO: Check if user has enough coins
+
         # Construct the POST object
         post_data = {
             "sender": self.user_id,
@@ -56,28 +78,50 @@ class User:
         else:
             print("Post failed.")
 
-    # TODO: method that displays user's current balance
-    # TODO: method that displays user's transactions
+    def get_transactions(self) -> list:
+        """Helper function for retrieving all transactions involving user."""
+        # Retrieve full chain
+        r = requests.get(url=self.url + "/chain")
+        # Parse into list of dictionaries
+        chain = r.json()["chain"]
+        # Iterate through chain, starting after last processed block
+        for block in chain[self.last_index :]:
+            # Iterate through each block's transactions
+            for transaction in block["transactions"]:
+                # Look for transactions where user_id is recipient or sender
+                if self.user_id in transaction.values():
+                    # Add transaction to user's transaction cache
+                    transaction["block_index"] = block["index"]
+                    transaction["block_timestamp"] = block["timestamp"]
+                    self.cache.append(transaction)
+                    # Update user's balance
+                    if transaction["recipient"] == self.user_id:
+                        self.balance += transaction["amount"]
+                    else:
+                        self.balance -= transaction["amount"]
+
+            self.last_index += 1  # Count block as processed
+
+        print(f"Updated wallet up to block {self.last_index + 1}")
 
 
-# %%
-james_bond = User()
-james_bond.user_id = "007"
-
-# %%
-jane_bond = User()
-jane_bond.user_id = "008"
-
-
-# %%
-# Post a new transaction
-james_bond.post_transaction("008", 1.8)
-jane_bond.post_transaction("007", 0.7)
-
-# %%
-jane_bond.post_transaction("007", 1.7)
-james_bond.post_transaction("008", 0.8)
-
-# %%
 if __name__ == "__main__":
-    user = User()
+    # === Instantiate users
+    james_bond = User(user_id="007", balance=100.0)
+    jane_bond = User(user_id="008", balance=50.0)
+
+    # === Update wallet
+    james_bond.get_transactions()
+    jane_bond.get_transactions()
+
+    # === Print balance
+    print(james_bond.balance)
+    print(jane_bond.balance)
+
+    # === Post a new transaction
+    james_bond.post_transaction("008", 1.8)
+    jane_bond.post_transaction("007", 0.7)
+
+    # === Post another transaction
+    jane_bond.post_transaction("007", 7.7)
+    james_bond.post_transaction("008", 8.8)
